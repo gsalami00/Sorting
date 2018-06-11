c5fb5011e6cac307cb706b2c559095100e02b458
Geraldine Salamat <g.a.salamat@gmail.com>
2018-06-11 14:55:20 -0400

    bubble sort

---
+++ bubblesort.spec.js
@@ -0,0 +1,32 @@
+describe('Bubble Sort', function() {
+  beforeAll(function () {
+    spyOn(bubbleSort, 'swap').and.callThrough(); // replace existing `tootsiepop['lick']` method
+  });
+  it('handles an empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it('handles an array of size 2', function() {
+    expect(bubbleSort([5, 4])).toEqual([4, 5]);
+    expect(bubbleSort.swap.calls.count()).toEqual(1);
+
+  });
+  it('handles an array of size n', function() {
+    expect(bubbleSort([4, 2, 7, 3, 1, 5])).toEqual([1, 2, 3, 4, 5, 7]);
+    expect(bubbleSort([2, 4, 7, 3, 1, 5, 6, 10, 9, 8])).toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+    ]);
+
+    
+    
+
+  });
+});
